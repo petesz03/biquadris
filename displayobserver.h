f48999ca86a85229a0ac4fc303ec3ea5f01a5381
p3qi <p3qi@uwaterloo.ca>
2022-12-02 08:43:37 -0500

    changeddisplayobserver.h

--- displayobserver.h
+++ displayobserver.h
@@ -1,10 +1,11 @@
 #ifndef _DISPLAYOBSERVER_H_
 #define _DISPLAYOBSERVER_H_
+
 class Subject;
 
 class DisplayObserver{
     public:
     virtual void notify() = 0;
-    virtual ~Observer() = default;
+    virtual ~DisplayObserver() = default;
 };
 #endif
