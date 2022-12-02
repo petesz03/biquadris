d46a1878b8c080931bab9755442d2eede2736db0
z34bai <z34bai@uwaterloo.ca>
2022-12-02 12:11:37 -0500

    remove .o

--- main.d
+++
@@ -1,3 +0,0 @@
-main.o: main.cc level.h player.h block.h posn.h board.h iblock.h jblock.h \
- lblock.h oblock.h sblock.h zblock.h tblock.h subject.h textdisplay.h \
- displayobserver.h graphicdisplay.h window.h
--- subject.d
+++
@@ -1,1 +0,0 @@
-subject.o: subject.cc subject.h displayobserver.h block.h posn.h
