b8e1f08ec8a688edef41a92a3a2d646b7bebda42
z34bai <z34bai@uwaterloo.ca>
2022-12-04 14:47:13 -0500

    charAt change

--- iblock.cc
+++ iblock.cc
@@ -17,9 +17,9 @@ void Iblock::clockwiseturn() {
         dir = 2;
         int x_val = box1.x;
         int y_val = box1.y;
-        char temp1 = the_board->charAt(x_val, y_val - 1);
-        char temp2 = the_board->charAt(x_val, y_val - 2);
-        char temp3 = the_board->charAt(x_val, y_val - 3);
+        char temp1 = the_board->charAt(y_val - 1, x_val);
+        char temp2 = the_board->charAt(y_val - 2, x_val);
+        char temp3 = the_board->charAt(y_val - 3, x_val);
         if (temp1 == ' ' && temp2 == ' ' && temp3 == ' ') {
             setbox2(x_val, y_val - 1);
             setbox3(x_val, y_val - 2);
@@ -29,9 +29,9 @@ void Iblock::clockwiseturn() {
         dir = 3;
         int x_val = box1.x;
         int y_val = box1.y;
-        char temp1 = the_board->charAt(x_val + 1, y_val);
-        char temp2 = the_board->charAt(x_val + 2, y_val);
-        char temp3 = the_board->charAt(x_val + 3, y_val);
+        char temp1 = the_board->charAt(y_val, x_val + 1);
+        char temp2 = the_board->charAt(y_val, x_val + 2);
+        char temp3 = the_board->charAt(y_val, x_val + 3);
         if (temp1 == ' ' && temp2 == ' ' && temp3 == ' ') {
             setbox2(x_val + 1, y_val);
             setbox3(x_val + 2, y_val);
@@ -41,9 +41,9 @@ void Iblock::clockwiseturn() {
         dir = 4;
         int x_val = box1.x;
         int y_val = box1.y;
-        char temp1 = the_board->charAt(x_val, y_val - 1);
-        char temp2 = the_board->charAt(x_val, y_val - 2);
-        char temp3 = the_board->charAt(x_val, y_val - 3);
+        char temp1 = the_board->charAt(y_val - 1, x_val);
+        char temp2 = the_board->charAt(y_val - 2, x_val);
+        char temp3 = the_board->charAt(y_val - 3, x_val);
         if (temp1 == ' ' && temp2 == ' ' && temp3 == ' ') {
             setbox2(x_val, y_val - 1);
             setbox3(x_val, y_val - 2);
@@ -53,9 +53,9 @@ void Iblock::clockwiseturn() {
         dir = 1;
         int x_val = box1.x;
         int y_val = box1.y;
-        char temp1 = the_board->charAt(x_val + 1, y_val);
-        char temp2 = the_board->charAt(x_val + 2, y_val);
-        char temp3 = the_board->charAt(x_val + 3, y_val);
+        char temp1 = the_board->charAt(y_val, x_val + 1);
+        char temp2 = the_board->charAt(y_val, x_val + 2);
+        char temp3 = the_board->charAt(y_val, x_val + 3);
         if (temp1 == ' ' && temp2 == ' ' && temp3 == ' ') {
             setbox2(x_val + 1, y_val);
             setbox3(x_val + 2, y_val);
@@ -67,10 +67,10 @@ void Iblock::clockwiseturn() {
                 drop();
                 return;
             } else {
-                char temp5 = the_board->charAt(box1.x, box1.y + 1);
-                char temp6 = the_board->charAt(box2.x, box2.y + 1);
-                char temp7 = the_board->charAt(box3.x, box3.y + 1);
-                char temp8 = the_board->charAt(box4.x, box4.y + 1);
+                char temp5 = the_board->charAt(box1.y + 1, box1.x);
+                char temp6 = the_board->charAt(box2.y + 1, box2.x);
+                char temp7 = the_board->charAt(box3.y + 1, box3.x);
+                char temp8 = the_board->charAt(box4.y + 1, box4.x);
                 if (temp5 == ' ' && temp6 == ' ' && temp7 == ' ' && temp8 == ' ') {
                     box1.y += 1;
                     box2.y += 1;
@@ -89,9 +89,9 @@ void Iblock::counterturn() {
         dir = 4;
         int x_val = box1.x;
         int y_val = box1.y;
-        char temp1 = the_board->charAt(x_val, y_val - 1);
-        char temp2 = the_board->charAt(x_val, y_val - 2);
-        char temp3 = the_board->charAt(x_val, y_val - 3);
+        char temp1 = the_board->charAt(y_val - 1, x_val);
+        char temp2 = the_board->charAt(y_val - 2, x_val);
+        char temp3 = the_board->charAt(y_val - 3, x_val);
         if (temp1 == ' ' && temp2 == ' ' && temp3 == ' ') {
             setbox2(x_val, y_val - 1);
             setbox3(x_val, y_val - 2);
@@ -101,9 +101,9 @@ void Iblock::counterturn() {
         dir = 1;
         int x_val = box1.x;
         int y_val = box1.y;
-        char temp1 = the_board->charAt(x_val + 1, y_val);
-        char temp2 = the_board->charAt(x_val + 2, y_val);
-        char temp3 = the_board->charAt(x_val + 3, y_val);
+        char temp1 = the_board->charAt(y_val, x_val + 1);
+        char temp2 = the_board->charAt(y_val, x_val + 2);
+        char temp3 = the_board->charAt(y_val, x_val + 3);
         if (temp1 == ' ' && temp2 == ' ' && temp3 == ' ') {
             setbox2(x_val + 1, y_val);
             setbox3(x_val + 2, y_val);
@@ -113,9 +113,9 @@ void Iblock::counterturn() {
         dir = 2;
         int x_val = box1.x;
         int y_val = box1.y;
-        char temp1 = the_board->charAt(x_val, y_val - 1);
-        char temp2 = the_board->charAt(x_val, y_val - 2);
-        char temp3 = the_board->charAt(x_val, y_val - 3);
+        char temp1 = the_board->charAt(y_val - 1, x_val);
+        char temp2 = the_board->charAt(y_val - 2, x_val);
+        char temp3 = the_board->charAt(y_val - 3, x_val);
         if (temp1 == ' ' && temp2 == ' ' && temp3 == ' ') {
             setbox2(x_val, y_val - 1);
             setbox3(x_val, y_val - 2);
@@ -125,9 +125,9 @@ void Iblock::counterturn() {
         dir = 3;
         int x_val = box1.x;
         int y_val = box1.y;
-        char temp1 = the_board->charAt(x_val + 1, y_val);
-        char temp2 = the_board->charAt(x_val + 2, y_val);
-        char temp3 = the_board->charAt(x_val + 3, y_val);
+        char temp1 = the_board->charAt(y_val, x_val + 1);
+        char temp2 = the_board->charAt(y_val, x_val + 2);
+        char temp3 = the_board->charAt(y_val, x_val + 3);
         if (temp1 == ' ' && temp2 == ' ' && temp3 == ' ') {
             setbox2(x_val + 1, y_val);
             setbox3(x_val + 2, y_val);
@@ -139,10 +139,10 @@ void Iblock::counterturn() {
                 drop();
                 return;
             } else {
-                char temp5 = the_board->charAt(box1.x, box1.y + 1);
-                char temp6 = the_board->charAt(box2.x, box2.y + 1);
-                char temp7 = the_board->charAt(box3.x, box3.y + 1);
-                char temp8 = the_board->charAt(box4.x, box4.y + 1);
+                char temp5 = the_board->charAt(box1.y + 1, box1.x);
+                char temp6 = the_board->charAt(box2.y + 1, box2.x);
+                char temp7 = the_board->charAt(box3.y + 1, box3.x);
+                char temp8 = the_board->charAt(box4.y + 1, box4.x);
                 if (temp5 == ' ' && temp6 == ' ' && temp7 == ' ' && temp8 == ' ') {
                     box1.y += 1;
                     box2.y += 1;
