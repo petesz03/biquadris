6865012414fc46e33ecf7694b084b30225f93a63
z34bai <z34bai@uwaterloo.ca>
2022-12-05 23:53:37 -0500

    fix command substring, specify block to be fixed

--- main.cc
+++ main.cc
@@ -30,26 +30,23 @@
 class BlockObserver;
 
 // We currently do not support seed
-int main(int argc, char** args){
+int main(int argc, char** args) {
     // Default is that we have a graphical display
     bool graphics = true;
     // The following two files are to replace level0's file:
     // The default is "sequencex.txt"
-    std::string filePlayer1 = "sequence1.txt"; 
+    std::string filePlayer1 = "sequence1.txt";
     std::string filePlayer2 = "sequence2.txt";
     int levelInitiated = 0;
-    for (int i = 1; i < argc; i++){
+    for (int i = 1; i < argc; i++) {
         std::string argument = args[i];
-        if (argument == "-text"){ graphics = false;}
-        else if (argument == "-scriptfile1"){
+        if (argument == "-text") { graphics = false; } else if (argument == "-scriptfile1") {
             i++;
             filePlayer1 = args[i];
-        }
-        else if (argument == "-scriptfile2"){
+        } else if (argument == "-scriptfile2") {
             i++;
             filePlayer2 = args[i];
-        }
-	else if (argument == "-startlevel"){
+        } else if (argument == "-startlevel") {
             i++;
             argument = args[i];
             levelInitiated = stoi(argument);
@@ -60,38 +57,34 @@ int main(int argc, char** args){
     Level* player1Level;
     Level* player2Level;
 
-    if (levelInitiated == 0){
-        player1Level = new Level0{filePlayer1};
-        player2Level = new Level0{filePlayer2};
-    }
-    else if (levelInitiated == 1){
+    if (levelInitiated == 0) {
+        player1Level = new Level0{ filePlayer1 };
+        player2Level = new Level0{ filePlayer2 };
+    } else if (levelInitiated == 1) {
         player1Level = new Level1{};
         player2Level = new Level1{};
-    }
-    else if (levelInitiated == 2){
+    } else if (levelInitiated == 2) {
         player1Level = new Level2{};
         player2Level = new Level2{};
-    }
-    else if (levelInitiated == 3){
+    } else if (levelInitiated == 3) {
         player1Level = new Level3{};
         player2Level = new Level3{};
-    }
-    else{
+    } else {
         player1Level = new Level4{};
         player2Level = new Level4{};
     }
-    
+
     /*** Don't touch: ***/
-    Player* player1 = new Player{1, player1Level, nullptr, nullptr, filePlayer1};
-    Board* player1Board = new Board{player1, player1Level};
+    Player* player1 = new Player{ 1, player1Level, nullptr, nullptr, filePlayer1 };
+    Board* player1Board = new Board{ player1, player1Level };
     player1->setBoard(player1Board);
-    Player* player2 = new Player{2, player2Level, nullptr, player1, filePlayer2};
-    Board* player2Board = new Board{player2, player2Level}; // Change depending on constructor of board
+    Player* player2 = new Player{ 2, player2Level, nullptr, player1, filePlayer2 };
+    Board* player2Board = new Board{ player2, player2Level }; // Change depending on constructor of board
     player2->setBoard(player2Board);
     /******/
 
     // Creating player1's text displays:
-    TextDisplay* textDisplay = new TextDisplay{player1Board, player2Board};
+    TextDisplay* textDisplay = new TextDisplay{ player1Board, player2Board };
 
 
     // If the filePlayer fields are not empty, we have to replace level0's file:
@@ -100,8 +93,8 @@ int main(int argc, char** args){
     // Creating player1 and player2's text displays:
 
     GraphicDisplay* graphicDisplay = nullptr;
-    if (graphics){
-        graphicDisplay = new GraphicDisplay{player1Board, player2Board};
+    if (graphics) {
+        graphicDisplay = new GraphicDisplay{ player1Board, player2Board };
     }
 
     // Since player1's opponent's pointer is nullptr, change the pointer:
@@ -116,211 +109,188 @@ int main(int argc, char** args){
     bool readFromFile = false;
     std::fstream commandfile;
     int repetition = 0;
-    while (true){
-	// Set who is moving for future commands:
-	Player* playerInPlay;
+    while (true) {
+        // Set who is moving for future commands:
+        Player* playerInPlay;
         Board* boardInPlay;
-        if (player1->getMyTurn()){
+        if (player1->getMyTurn()) {
             playerInPlay = player1;
             boardInPlay = player1->getBoard();
-        }
-        else{
+        } else {
             playerInPlay = player2;
             boardInPlay = player2->getBoard();
         }
-	// If either's turn is finished, it is other's term	
-	if (player1->getIsOver() && player1->getIsOver()){
-		break;
-	}
-	else if (player1->getIsOver()){
-		playerInPlay = player2;
-		boardInPlay = player2->getBoard();
-	}
-	else if (player2->getIsOver()){
-		playerInPlay = player1;
-		boardInPlay = player1->getBoard();
-	}
+        // If either's turn is finished, it is other's term	
+        std::cout << "[over]" << player1->getIsOver() << player2->getIsOver() << std::endl;
+        if (player1->getIsOver() && player2->getIsOver()) {
+            break;
+        } else if (player1->getIsOver()) {
+            playerInPlay = player2;
+            boardInPlay = player2->getBoard();
+        } else if (player2->getIsOver()) {
+            playerInPlay = player1;
+            boardInPlay = player1->getBoard();
+        }
 
-	if (repetition == 0){
-        	if (!readFromFile){
-            		if (std::cin >> command){}
-            		else{ break; }
-        	}
-        	else{
-            		if (commandfile >> command){}
-            		else{ std::cin >> command; }
-		}
-		// Read for input and call functions in playerInPlay,
-        	//   WHILE checking for similar commands:
+        if (repetition == 0) {
+            if (!readFromFile) {
+                if (std::cin >> command) {} else { break; }
+            } else {
+                if (commandfile >> command) {} else { std::cin >> command; }
+            }
 
-        	// We check for similar commands that "command" matches to:
-        	int commandsMatched = 0;
-        	std::string commandToExecute = "";
+            std::cout << "[command] " << command << std::endl;
+            // Read for input and call functions in playerInPlay,
+                //   WHILE checking for similar commands:
 
-        	int commandDigit = 0;
-        	for (size_t i = 0; i <= command.size(); i++){
-        	        if (isdigit(command[i])){
-                	        commandDigit++;
-                	}
-               		 else{ break; }
-        	}
-		if (commandDigit == 0){
-			repetition = 1;
-		}
-		else{
-			repetition = stoi(command.substr(0, commandDigit));
-        		command = command.substr(commandDigit, command.size() - 1);	
-		}
-		std::vector<std::string> listOfCommands = {"left", "right", "down", "clockwise",            "counterclockwise", "drop", "levelup", "leveldown", "norandom", "random",            "sequence", "I", "J", "L", "restart"};
-		// Check how many different types of command that "command" matches to:
-		for (auto it = listOfCommands.begin(); it != listOfCommands.end(); it++){            
-			size_t matched = (*it).find(command);
-			if (matched != std::string::npos){
-				commandsMatched++;
-				commandToExecute = (*it);
-	    		}
-		}
-		if (commandsMatched == 1){
-			command = commandToExecute;
-		}
-	}
-        if (command == "left"){
+                // We check for similar commands that "command" matches to:
+            int commandsMatched = 0;
+            std::string commandToExecute = "";
+
+            int commandDigit = 0;
+            for (size_t i = 0; i <= command.size(); i++) {
+                if (isdigit(command[i])) {
+                    commandDigit++;
+                } else { break; }
+            }
+            if (commandDigit == 0) {
+                repetition = 1;
+            } else {
+                repetition = stoi(command.substr(0, commandDigit));
+                command = command.substr(commandDigit, command.size() - 1);
+            }
+            std::vector<std::string> listOfCommands = {
+                "left", "right", "down", "clockwise","counterclockwise", "drop", "levelup", "leveldown", 
+                "norandom", "random","sequence", "I", "J", "L", "S", "Z", "T", "O", "restart"
+                };
+            // Check how many different types of command that "command" matches to:
+            for (auto it = listOfCommands.begin(); it != listOfCommands.end(); it++) {
+                if ((*it).find(command, 0) != std::string::npos) {
+                    commandsMatched++;
+                    commandToExecute = (*it);
+                }
+            }
+            if (commandsMatched == 1) {
+                command = commandToExecute;
+            } else {
+                std::cout << "Invalid Command" << std::endl;
+                repetition = 0;
+                continue;
+            }
+        }
+
+        std::cout << "[commandlength] " << command.length() << std::endl;
+        if (command == "left") {
             playerInPlay->makeMoveLeft();
-        }
-        else if (command == "right"){
+        } else if (command == "right") {
             playerInPlay->makeMoveRight();
-        }
-        else if (command == "down"){
+        } else if (command == "down") {
             playerInPlay->makeMoveDown();
-        }
-        else if (command == "clockwise"){
+        } else if (command == "clockwise") {
             playerInPlay->makeClockwiseTurn();
-        }
-        else if (command == "counterclockwise"){
+        } else if (command == "counterclockwise") {
             playerInPlay->makeCounterTurn();
-        }
-        else if (command == "drop"){
+        } else if (command == "drop") {
             playerInPlay->makeDrop();
-	    if (playerInPlay->getIsOver()){
-		    (player1->getBoard())->render();
-		    (player2->getBoard())->render();
-		    break;
-	    }
-        }
-        else if (command == "levelup"){
+            if (playerInPlay->getIsOver()) {
+                (player1->getBoard())->render();
+                (player2->getBoard())->render();
+                break;
+            }
+        } else if (command == "levelup") {
             playerInPlay->levelUp();
-        }
-        else if (command == "leveldown"){
+        } else if (command == "leveldown") {
             playerInPlay->levelDown();
-        }
-        else if (command == "norandom"){
+        } else if (command == "norandom") {
             playerInPlay->setIsRandom(false);
-	    std::string file;
-	    std::cin >> file;
-	    (playerInPlay->myLevel)->useFile(file);
-        }
-        else if (command == "random"){
-            playerInPlay->setIsRandom(true);
-        }
-        else if (command == "sequence"){
             std::string file;
-            if (!readFromFile){ std::cin >> file; }
-            else{
+            std::cin >> file;
+            (playerInPlay->myLevel)->useFile(file);
+        } else if (command == "random") {
+            playerInPlay->setIsRandom(true);
+        } else if (command == "sequence") {
+            std::string file;
+            if (!readFromFile) { std::cin >> file; } else {
                 // If we are currently already reading from a file,
                 //   need to close it before before opening a new one:
                 commandfile >> file;
                 commandfile.close();
-                }
+            }
             readFromFile = true;
             commandfile.open(file, std::fstream::in);
-        }
-        else if (command == "I"){
-            	Block* curBlock = boardInPlay->getCurrentBlock();
-		boardInPlay->detach(curBlock);
-            	// Delete the old "currentBlock" in Board:
-	    	delete curBlock;
-	    	// Create new Block
-	    	Block* newblock = new Iblock{boardInPlay};
-		boardInPlay->attach(curBlock);
-	    	// Set currentBlock in Board
-	    	boardInPlay->setCurrent(newblock); 
-        }
-	else if (command == "J"){                
-		Block* curBlock = boardInPlay->getCurrentBlock();
-		boardInPlay->detach(curBlock);
-		// Delete the old "currentBlock" in Board:
-		delete curBlock;
-		// Create new Block
-		Block* newblock = new Jblock{boardInPlay};	
-            	boardInPlay->attach(curBlock);
-		// Set currentBlock in Board                
-		boardInPlay->setCurrent(newblock);
-	    }
-	else if (command == "L"){
-		Block* curBlock = boardInPlay->getCurrentBlock();
-		boardInPlay->detach(curBlock);
-		// Delete the old "currentBlock" in Board:
-            	delete curBlock;
-            	// Create new Block
-            	Block* newblock = new Lblock{boardInPlay};
-		boardInPlay->attach(curBlock);
-            	// Set currentBlock in Board
-            	boardInPlay->setCurrent(newblock);
-        }
-        else if (command == "restart"){
-		// Delete old displays in main to avoid
-		//   multiple deletes of the same displays
-		(player1->getBoard())->detach(graphicDisplay);
-		(player1->getBoard())->detach(textDisplay);
-		delete graphicDisplay;
-		delete textDisplay;
+        } else if (command == "restart") {
+            // Delete old displays in main to avoid
+            //   multiple deletes of the same displays
+            (player1->getBoard())->detach(graphicDisplay);
+            (player1->getBoard())->detach(textDisplay);
+            delete graphicDisplay;
+            delete textDisplay;
 
-		// Get new boards for player1 and player2:
-		player1->restart();
-		player2->restart();
+            // Get new boards for player1 and player2:
+            player1->restart();
+            player2->restart();
 
-		// Build new displays:
-		GraphicDisplay* newGraphics = new GraphicDisplay{player1->getBoard(), player2->getBoard()};
-		TextDisplay* newText = new TextDisplay{player1->getBoard(), player2->getBoard()};
-		// Reassign the textDisplay and graphicDisplay
-		textDisplay = newText;
-		graphicDisplay = newGraphics;
+            // Build new displays:
+            GraphicDisplay* newGraphics = new GraphicDisplay{ player1->getBoard(), player2->getBoard() };
+            TextDisplay* newText = new TextDisplay{ player1->getBoard(), player2->getBoard() };
+            // Reassign the textDisplay and graphicDisplay
+            textDisplay = newText;
+            graphicDisplay = newGraphics;
 
-		// Call the restart function to get new boards for player 1 and 2.
+            // Call the restart function to get new boards for player 1 and 2.
 
-		// Attach the new displays:
-		(player1->getBoard())->attach(newGraphics);
-		(player2->getBoard())->attach(newGraphics);
-		(player1->getBoard())->attach(newText);
-		(player2->getBoard())->attach(newText);
+            // Attach the new displays:
+            (player1->getBoard())->attach(newGraphics);
+            (player2->getBoard())->attach(newGraphics);
+            (player1->getBoard())->attach(newText);
+            (player2->getBoard())->attach(newText);
+        } else if (command == "newblock") {
+            // Testing functions: (delete in end)
+            Block* newblock = new Iblock{};
+        } else if (command == "render") {
+            if (player1->getMyTurn()) { std::cout << "Player 1 moving" << std::endl; } else if (player2->getMyTurn()) { std::cout << "Player2 moving" << std::endl; } else { std::cout << "What is going on" << std::endl; }
+            (player1->getBoard())->render();
+            (player2->getBoard())->render();
+        } else if (command.length() == 1) {
+            // Create the specified block
+            Block* newblock = nullptr;
+            if (command == "I"){
+                newblock = new Iblock{ boardInPlay };
+            } else if (command == "J"){
+                newblock = new Jblock{ boardInPlay };
+            } else if (command == "L"){
+                newblock = new Lblock{ boardInPlay };
+            } else if (command == "S"){
+                newblock = new Sblock{ boardInPlay };
+            } else if (command == "Z"){
+                newblock = new Zblock{ boardInPlay };
+            } else if (command == "T"){
+                newblock = new Tblock{ boardInPlay };
+            } else if (command == "O"){
+                newblock = new Oblock{ boardInPlay };
+            }
+            Block* curBlock = boardInPlay->getCurrentBlock();
+            boardInPlay->detach(curBlock);
+            // Delete the old "currentBlock" in Board:
+            delete curBlock;
+            boardInPlay->attach(curBlock);
+            // Set currentBlock in Board
+            boardInPlay->setCurrent(newblock);
         }
-	// Testing functions: (delete in end)
-	else if (command == "newblock"){
-		Block* newblock = new Iblock{};
-	}
-	else if (command == "render"){
-		if (player1->getMyTurn()){ std::cout << "Player 1 moving" << std::endl; }
-		else if (player2->getMyTurn()){ std::cout << "Player2 moving" << std::endl; }
-		else{ std::cout << "What is going on" << std::endl; }
-		(player1->getBoard())->render();
-		(player2->getBoard())->render();
-	}
-	repetition--;
-		
+        repetition--;
+        std::cout << "[repetition] " << repetition << std::endl;
     }
-                int player1HighScore = player1->getMaxScore();
-		int player2HighScore = player2->getMaxScore(); 
- 		std::cout << "Player 1's High Score is: " << player1HighScore << std::endl;
-		std::cout << "Player 2's High Score is: " << player2HighScore << std::endl;
-                if (player1HighScore > player2HighScore){
-                        std::cout << "Player 1 win!" << std::endl;
-                }
-                else if (player2HighScore > player1HighScore){
-                        std::cout << "Player 2 win!" << std::endl;
-                }
-                else{
-                        std::cout << "Tie!" << std::endl;
-                }
+    int player1HighScore = player1->getMaxScore();
+    int player2HighScore = player2->getMaxScore();
+    std::cout << "Player 1's High Score is: " << player1HighScore << std::endl;
+    std::cout << "Player 2's High Score is: " << player2HighScore << std::endl;
+    if (player1HighScore > player2HighScore) {
+        std::cout << "Player 1 win!" << std::endl;
+    } else if (player2HighScore > player1HighScore) {
+        std::cout << "Player 2 win!" << std::endl;
+    } else {
+        std::cout << "Tie!" << std::endl;
+    }
 
     /***** Free Memory: *****/
     // Deleting players will delete its corresponding level and board, which
